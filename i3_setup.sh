75a9cfce1711d68fde8e5b344e0afb42374b14f4
Ari Abramowitz <ari.abramowitz1@gmail.com>
2022-08-19 11:17:28 -0400

    Add i3, i3blocks, i3-gnome

--- i3_setup.sh
+++ i3_setup.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 sudo apt install i3
+
+# i3
+mkdir ~/.config/i3
+git clone git@github.com:AriAbr/i3-setup.git ~/.config/i3
+sudo apt install i3status suckless-tools i3blocks rofi arandr -y
+
+# i3blocks
+sudo apt install fonts-font-awesome acpi -y
+mkdir ~/.config/i3blocks
+git clone git@github.com:AriAbr/i3blocks-setup.git ~/.config/i3blocks
+
+
+# i3 gnome
+git clone https://github.com/i3-gnome/i3-gnome.git
+cd i3-gnome
+sudo make install
+cd ..
+rm -rf i3-gnome

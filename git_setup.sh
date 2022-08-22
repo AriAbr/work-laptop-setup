f937be7f23d0a1b7ea8f82a118c26d8a0d61e15a
Ari Abramowitz <ari.abramowitz1@gmail.com>
2022-08-22 16:17:16 -0400

    Move user from global to local gitconfig

--- git_setup.sh
+++ git_setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 sudo apt install git -y
-git config --global user.name "Ari Abramowitz"
-git config --global user.email "ari.abramowitz1@gmail.com"
 
 cp ./.gitconfig.local ~/.gitconfig.local

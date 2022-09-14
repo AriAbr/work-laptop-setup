20f40948875ffedeb0c2ae9caa7b85d6890f3cbe
Ari Abramowitz <ari.abramowitz1@gmail.com>
2022-09-14 11:59:14 +0300

    Initial Sites setup script

--- setup_sites.sh
+++ setup_sites.sh
@@ -4,16 +4,15 @@
 cd ~/dev
 
 # Clone the Project (with ssh, change the link if you want https)
-git clone git@github.com:Centers-Health/dynamics-cas.git
-cd ~/dev/dynamics-cas
+git clone git@github.com:Centers-Health/centers-sites.git
+cd ~/dev/centers-sites
 
+# create a .env file...
 touch .env
-
-read  -n 1 -p "Copy the CAS env, then press ENTER to continue..." $foo
+read  -n 1 -p "Copy the Sites env, then press ENTER to continue..." $foo
 
 # Install python dependencies
 dm poetry install
-
 
 # Install and setup pre-commit hooks
 pre-commit install -t pre-commit -t pre-push
@@ -21,6 +20,8 @@ pre-commit install -t pre-commit -t pre-push
 # run migrations
 dm manage migrate
 
+# Create super user
+# recommendation: username: docker, password: docker
 # Create superuser. based on https://stackoverflow.com/a/26091252
 USERNAME="docker"
 EMAIL="docker@docker.com"
